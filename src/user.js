3187536f13f9466bbc5fe469c97d4c48766fcf7d
adityasam612 <81520351+adityasam612@users.noreply.github.com>
2022-01-13 15:40:27 +0530

    Update user.js

--- src/user.js
+++ src/user.js
@@ -1,18 +1,20 @@
 import GUN from 'gun';
-import 'gun/sea';
-import 'gun/axe';
-import { writable } from 'svelte/store';
+import 'gun/sea'; // security encryption and authorization  
+import 'gun/axe'; // altternative way to connect peers together 
+import { writable } from 'svelte/store'; // too rerender and to share widely & to reatice also 
 
-// Database
-export const db = GUN();
+// Database 
+export const db = GUN(); //initialize the database
 
 // Gun User
-export const user = db.user().recall({sessionStorage: true});
+export const user = db.user().recall({sessionStorage: true}); // so the user stays logged in between  browser sessions 
 
 // Current User's username
 export const username = writable('');
 
-user.get('alias').on(v => username.set(v))
+user.get('alias').on(v => username.set(v)) // which will be the value of username
+
+// to cheack the auth state when the user signs in or out 
 
 db.on('auth', async(event) => {
     const alias = await user.get('alias'); // username string
